bbe404e90eaba42133155677b291fdb09883b4f0
whimsy <whimsy@sylph.box>
2025-04-16 08:04:46 -0500

    • Enabled alpha matting in main.py to help alleviate smudging issues.

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ def main():
     with open(input_path, "rb") as input_file:
         input_data = input_file.read()
 
-    result = remove(input_data)
+    result = remove(
+        input_data,
+        alpha_matting=True,
+        alpha_matting_foreground_threshold=240,
+        alpha_matting_background_threshold=10,
+        alpha_matting_erode_size=10
+    )
 
     with open(output_path, "wb") as output_file:
         output_file.write(result)
